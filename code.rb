c5eb5e9ca3e62c84fe28d5b08ff8f61df31a7454
José Galisteo Ruiz <ceritium@gmail.com>
2010-11-30 19:45:55 +0100

    Refactorizando, gracias jrdi

--- code.rb
+++ code.rb
@@ -6,9 +6,6 @@ a = [69, 60, 59, 72, 74, 55, 10, 8, 22, 60, 66, 69, 77, 59, 72, 73, 63, 68, 73, 
 p "Answer to the Ultimate Question of Life, the Universe, and Everything"
 b = gets.chomp.to_i
 
-c = []
-a.each do |d|
-  c << (d + b).chr
-end     
+c = a.map{ |d| (d + b).chr }.join('')
 
-puts c.join('')
+puts c
